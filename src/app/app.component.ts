1bd0d87a90ba70b78b645180fcd84b5e3752f194
Sayed Hussainullah Sadat <sayedsadat344@gmail.com>
2022-06-13 16:21:02 +0430

    charts.js usage with angular 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-chartjs-tutorial';
+
+  /**
+   *
+   * Make components of type LINE, PIE, BAR
+   * Make senarioa for each graph
+   * Make data format for back-end compatibality
+   *
+   */
 }
